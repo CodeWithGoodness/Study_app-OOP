6cb6307df1ddc0b32405d8f16c5f787a17807bd9
CodeWithGoodness <ceciliaOkoro2@gmail.com>
2022-05-26 17:13:56 -0700

    first commit

--- src/Courses.java
+++ src/Courses.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Courses {
-   static ArrayList<String> subjects = new ArrayList<>();
+   public static List<String> subjects = new ArrayList<>();
     public static void addCourse (){
         subjects.add("CSC 201");
         subjects.add("Gst 201");
--- src/GpCalculator.java
+++ src/GpCalculator.java
@@ -2,48 +2,46 @@ import java.util.*;
 public class GpCalculator extends Courses{
     static String gradeUnit, courseUnit;
     Integer GP;
-    private static ArrayList<String> grades = new ArrayList<>();
+    //private static ArrayList<String> grades = new ArrayList<>();
     private static ArrayList<Integer> units = new ArrayList<>();
     public static void myCourses() {
     }
     public static void DisplayCoursesAndUnits(){
+        getGP();
+    }
+    public static Integer getGradesUnits() {
         addCourse();
         System.out.println("Enter your grades");
         Scanner input = new Scanner(System.in);
+        String grades = " ";
         for (int i = 0; i < 2; i++) {
             System.out.println(subjects.get(i));
-            grades.add(input.next());
+             grades = (input.next());
         }
-        getGP();
-    }
-    public static Integer getGradesUnits(){
         HashMap<String, Integer> gradeUnit = new HashMap<String, Integer>();
         gradeUnit.put("A", 12);
         gradeUnit.put("B", 10);
         gradeUnit.put("C", 6);
         gradeUnit.put("D", 4);
-        while (gradeUnit.containsKey(grades));
-        return gradeUnit.get(grades);
+        for (int i = 0; ; i++) {
+            return gradeUnit.get(grades);
+        }
+
     }
     public static Integer getCourseUnit(){
         HashMap<String, Integer> courseUnit = new HashMap<String, Integer>();
         courseUnit.put("CSC 201", 3);
         courseUnit.put("Gst 201", 2);
-        int i =0;
         System.out.println("Hey");
-        while (courseUnit.containsKey(subjects.get(i)));
-        return courseUnit.get(subjects.get(i));
+        for (in t i = 0; ; i++) {
+            return courseUnit.get(subjects.get(i));
+        }
     }
     public static void getGP(){
         getGradesUnits();
-        System.out.println("Hey");
         getCourseUnit();
-        System.out.println("Hey");
-        for (int i = 0; i < 2; i++) {
+       System.out.println("Hey");
             System.out.println(getGradesUnits() * getCourseUnit());
-            System.out.println("Hey");
-        }
 
     }
-
 }
